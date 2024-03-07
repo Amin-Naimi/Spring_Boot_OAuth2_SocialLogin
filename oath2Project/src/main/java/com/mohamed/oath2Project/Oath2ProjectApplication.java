91d424e22d9271709435c97f51005ba191419d60
Mohamed <ahnbi32@gmail.com>
2024-03-07 11:29:58 +0100

    First commit

---
+++ oath2Project/src/main/java/com/mohamed/oath2Project/Oath2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.oath2Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2ProjectApplication.class, args);
+	}
+
+}
---
+++ oath2Project/src/test/java/com/mohamed/oath2Project/Oath2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.oath2Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
